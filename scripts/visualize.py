6a882bae18a73a3acb4d5c805f85c927e6478be5
Joseph <joseph.hung@mail.utoronto.ca>
2020-04-17 23:25:26 -0400

    I think all plots are in order?

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -40,46 +40,51 @@ model_600_150epoch = pickle.load(file_handler_2)
 file_handler_3 = open('./pickle/1000_350epoch.obj','rb')
 model_1000_350epoch = pickle.load(file_handler_3)
 
+file_handler_5 = open('./pickle/1000_700epoch.obj','rb')
+model_1000_700epoch = pickle.load(file_handler_5)
+
 #Compile pairs for testing
 test_x, test_y = compile_pairs(testing_data,in_profile,out_profile)
 grid = np.linspace(0,15000,500)
 
-pred_350, pred_150, = [], []
+pred_350, pred_150, pred_700 = [], [], []
 
 
 #Reshape testing pairs and run through model
 for sett in test_x:
     pred_350.append(model_1000_350epoch.predict(sett.reshape((1,in_profile,in_dim))))
     pred_150.append(model_600_150epoch.predict(sett.reshape((1,in_profile,in_dim))))
+    pred_700.append(model_1000_700epoch.predict(sett.reshape((1,in_profile,in_dim))))
 
 #Plot comarisons between different training lengths
 for i, inp in enumerate(pred_350):
     plt.title("Comparison between True Profile and Generated Profiles at time " + str(i))
-    plt.plot(inp.reshape((500,)),grid, marker='1',label="350_epoch_trained")
+    plt.plot(inp.reshape((500,)),grid,label="350_epoch_trained", ls='--')
+    plt.plot(pred_150[i].reshape((500,)),grid,label="150_epoch_trained",ls='--')
+    plt.plot(pred_700[i].reshape((500,)),grid,label="700_epoch_trained")
+    plt.plot(test_y[i].reshape((500,)),grid,label="actual")
     plt.xlabel("WVMR")
     plt.ylabel("Altitude (m)")
-    plt.plot(pred_150[i].reshape((500,)),grid, marker='2',label="150_epoch_trained")
-    plt.plot(test_y[i].reshape((500,)),grid,label="actual")
     plt.legend()
-    plt.savefig("./plots/"+ str(i)+ "_predicted.png")
+    plt.savefig("./plots/700_epoch/Predicted/"+ str(i)+ "_predicted.png")
     plt.clf()
-
 
 #Now use the generated images to forecast beyond a single point
 predicted_set = training_data[:30]
-window = predicted_set
+window1 = predicted_set
+window2 = predicted_set
 for i in range(len(testing_data)):
     print(i)
-    predicted = model_1000_350epoch.predict(window.reshape((1,in_profile,in_dim)))
-    print("new", np.shape(window))
-    print(np.shape(predicted))
-    window = np.append(window[1:], predicted.reshape((1,in_dim)),axis=0)
-    print(np.shape(window))
+    predicted1 = model_1000_700epoch.predict(window1.reshape((1,in_profile,in_dim)))
+    predicted2 = model_1000_350epoch.predict(window2.reshape((1,in_profile,in_dim)))
+    window1 = np.append(window1[1:], predicted1.reshape((1,in_dim)),axis=0)
+    window2 = np.append(window2[1:], predicted2.reshape((1,in_dim)),axis=0)
     plt.title("Comparison between True Profile and Forecasted Profiles at time " + str(i))
-    plt.plot(predicted.reshape((500,)),grid, marker='1',label="Forecasted")
+    plt.plot(predicted1.reshape((500,)),grid,label="Forecasted_700_Epoch")
+    plt.plot(predicted2.reshape((500,)),grid, ls='--',label="Forecasted_350_Epoch")
     plt.xlabel("WVMR")
     plt.ylabel("Altitude (m)")
     plt.plot(testing_data[i].reshape((500,)),grid,label="Actual")
     plt.legend()
-    plt.savefig("./plots/forecasted/"+ str(i)+ "_forecasted.png")
+    plt.savefig("./plots/700_epoch/Forecasted/"+ str(i)+ "_forecasted.png")
     plt.clf()
